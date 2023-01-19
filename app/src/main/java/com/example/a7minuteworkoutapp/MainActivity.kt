663aaa3f9ba71080e91f49d1e2073d57c758ed6b
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-01-20 00:27:03 +0800

    created main page UI

--- app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStart : FrameLayout = findViewById(R.id.flStart)
+        flStart.setOnClickListener{
+            Toast.makeText(this, "Start!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
