fe0b6aeb8011047efd84e43b16346984dd34cc62
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-01-27 18:22:28 +0800

    added view binding support

--- app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.a7minuteworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStart : FrameLayout = findViewById(R.id.flStart)
-        flStart.setOnClickListener{
+//        val flStart : FrameLayout = findViewById(R.id.flStart) use view binding instead
+
+        binding?.flStart?.setOnClickListener{
             Toast.makeText(this, "Start!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
