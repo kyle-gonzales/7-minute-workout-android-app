caeae6cb5c9105b63b4d009030f4c66b0517bfbf
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-02-03 18:30:01 +0800

    fixed logic of bmi calculator

--- app/src/main/java/com/example/a7minuteworkoutapp/BmiActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/BmiActivity.kt
@@ -10,8 +10,8 @@ import com.example.a7minuteworkoutapp.databinding.ActivityBmiBinding
 class BmiActivity : AppCompatActivity() {
     private var binding : ActivityBmiBinding? = null
     private var bmi : Double = 0.0
-    var weight : Double = 0.0
-    var height : Double = 0.0
+    private var weight : Double = 0.0
+    private var height : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,30 +25,32 @@ class BmiActivity : AppCompatActivity() {
         binding?.toolbar?.setNavigationOnClickListener {
             finish()
         }
-        try{
-
-        weight = binding?.etWeight?.text.toString().toDouble()
-        height = binding?.etHeight?.text.toString().toDouble()
-        } catch (e: Exception) {
-            Log.e("EDIT", e.toString())
-            Toast.makeText(this@BmiActivity, "error", Toast.LENGTH_SHORT).show()
-        }
 
         binding?.btnCalculate?.setOnClickListener {
-            bmi = calculateBmi(weight, height)
+            try{
+
+                weight = binding?.etWeight?.text.toString().toDouble()
+                height = binding?.etHeight?.text.toString().toDouble()
+            } catch (e: Exception) {
+                Log.e("EDIT", e.toString())
+                Toast.makeText(this@BmiActivity, "error", Toast.LENGTH_SHORT).show()
+            }
+            bmi = calculateBmi(weight, height).toDouble()
             binding?.tvBmi?.text = bmi.toString()
+
             binding?.tvDiagnosis?.text = diagnoseBmi(bmi)
             binding?.tvMessage?.text = adviseBmi(bmi)
             binding?.llBmiResult?.visibility = View.VISIBLE
         }
     }
 
-    private fun calculateBmi(weight: Double, height: Double): Double{
-        return try{
-//            String.format(".%2d", weight / (height/100 * height/100)).toDouble()
-            weight / (height/100 * height/100)
-        } catch(e : ArithmeticException) {
-            -1.0
+    private fun calculateBmi(weight: Double, height: Double): Double {
+        return try {
+            String.format("%.2f", (weight / (height / 100 * height / 100))).toDouble()
+        } catch (e: ArithmeticException) {
+            1.0
+        } catch (e: Exception) {
+            1.0
         }
     }
 
@@ -67,10 +69,10 @@ class BmiActivity : AppCompatActivity() {
     private fun adviseBmi(bmi: Double): String{
 
         return when(bmi){
-            in 0.0..18.49 -> "Oops! You should exercise and increase your food intake!"
-            in 18.5..24.99 -> "You are doing great! Keep exercising to feel even better!"
-            in 25.0..29.9 -> "Oops! You really need to take care of yourself! Workout maybe!"
-            in 30.0..Double.MAX_VALUE -> "You need to take more serious action in taking care of your health!"
+            in 0.0..18.49 -> "You are ugly af. hit the gym bitch"
+            in 18.5..24.99 -> "I've seen skeletons with more muscle than you"
+            in 25.0..29.9 -> "You are still ugly af. hit the gym bitch"
+            in 30.0..Double.MAX_VALUE -> "damn"
             else -> "User input is invalid! Check your input and try again!"
         }
 
